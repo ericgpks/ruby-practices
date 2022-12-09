837a44e117f21ad6ec5452c25e0c3df9caf3ec38
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-09 11:52:33 +0900

    fix: recreate contents simple

--- 02.calendar/main.rb
+++ 02.calendar/main.rb
@@ -1,30 +1,49 @@
 require 'date'
+require 'optparse'
 
-# year と month を受け取る
-DAY = %w[日 月 火 水 木 金 土]
+opt = OptionParser.new
+opt.on('-m') { |v| p v }
+opt.on('-y') { |v| p v }
+opt.parse!(ARGV)
 
-year = Date.today.year
-month = Date.today.month
-cwday = Date.new(year, month, 1).cwday
-last_date = Date.new(year, month, -1).day
+def create_calender
+  calender = ""
 
-week = []
-start_date = 1
-week_count = 0
 
-# 1週間分を作るための for
-(start_date..last_date).each do|day|
-  if day == 1 && cwday != 7
-    cwday.times do
-      week << " "
-      week_count += 1
+  year = Date.today.year
+  month = Date.today.month
+
+  calender += "       #{ month.to_s.rjust(2) }月 #{ year }\n"
+
+  days = %w[日 月 火 水 木 金 土]
+  days.each { |day|
+    calender += " " + day.to_s
+  }
+  calender += "\n"
+
+  start_date = 1
+  cwday = Date.new(year, month, start_date).cwday
+  last_date = Date.new(year, month, -1).day
+  today = Date.today
+
+  (start_date..last_date).each do|day|
+    if day == 1 && cwday != 7
+      cwday.times do
+        calender += " " + " ".rjust(2)
+      end
+    end
+    if Date.new(year, month, day) == today
+      calender += " " + "\e[35m#{ day.to_s.rjust(2) }\e[0m"
+    else
+      calender += " " + day.to_s.rjust(2)
+    end
+
+    if Date.new(year, month, day).cwday == 6
+      calender += "\n"
     end
   end
-  week << day
-  week_count += 1
-  if week_count == 7
-    puts week.to_s
-    week_count = 0
-    week = []
-  end
+
+  calender
 end
+
+puts create_calender
