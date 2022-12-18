82685d53f3fe8161ec434eb77f3be0e086a67843
ericgpks <sq.wacbc5.nyc@gmail.com>
2022-12-18 11:56:30 +0900

    fix: change scores to instance variable

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -6,15 +6,15 @@ require 'debug'
 
 STRIKE = 'X'
 
-def main(scores, frame_count, ball_count, total_score)
-  scores.each_with_index do |score, index|
+def main(frame_count, ball_count, total_score)
+  @scores.each_with_index do |score, index|
     if frame_count == 10
-      total_score = calc_last_frame(total_score, scores, score, index)
+      total_score = calc_last_frame(total_score, score, index)
       break
     end
     if ball_count == 1
       if score == STRIKE
-        total_score, frame_count = calc_strike(total_score, frame_count, scores, index)
+        total_score, frame_count = calc_strike(total_score, frame_count, index)
       else
         total_score += score.to_i
         ball_count += 1
@@ -22,7 +22,7 @@ def main(scores, frame_count, ball_count, total_score)
       next
     end
 
-    total_score = calc_spare(total_score, scores, index) if (score.to_i + scores[index - 1].to_i) == 10
+    total_score = calc_spare(total_score, index) if (score.to_i + @scores[index - 1].to_i) == 10
     total_score += score.to_i
     ball_count = 1
     frame_count += 1
@@ -43,28 +43,28 @@ end
 
 private
 
-def calc_last_frame(total_score, scores, score, index)
+def calc_last_frame(total_score, score, index)
   total_score += score == STRIKE ? 10 : score.to_i
-  total_score += scores[index + 1] == STRIKE ? 10 : scores[index + 1].to_i
-  unless scores[index + 2].nil?
-    total_score += scores[index + 2] == STRIKE ? 10 : scores[index + 2].to_i
+  total_score += @scores[index + 1] == STRIKE ? 10 : @scores[index + 1].to_i
+  unless @scores[index + 2].nil?
+    total_score += @scores[index + 2] == STRIKE ? 10 : @scores[index + 2].to_i
   end
   total_score
 end
 
-def calc_strike(total_score, frame_count, scores, index)
+def calc_strike(total_score, frame_count, index)
   total_score += 10
-  total_score += scores[index + 1] == STRIKE ? 10 : scores[index + 1].to_i
-  total_score += scores[index + 2] == STRIKE ? 10 : scores[index + 2].to_i
+  total_score += @scores[index + 1] == STRIKE ? 10 : @scores[index + 1].to_i
+  total_score += @scores[index + 2] == STRIKE ? 10 : @scores[index + 2].to_i
   frame_count += 1
   [total_score, frame_count]
 end
 
-def calc_spare(total_score, scores, index)
-  total_score += scores[index + 1] == STRIKE ? 10 : scores[index + 1].to_i
+def calc_spare(total_score, index)
+  total_score += @scores[index + 1] == STRIKE ? 10 : @scores[index + 1].to_i
   total_score
 end
 
-scores, frame_count, ball_count, total_score = setup
-total_score = main(scores, frame_count, ball_count, total_score)
+@scores, frame_count, ball_count, total_score = setup
+total_score = main(frame_count, ball_count, total_score)
 puts total_score
