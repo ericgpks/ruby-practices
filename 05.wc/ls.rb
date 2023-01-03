3fde7185c8e31706a1449fc665e020598fbfe1b7
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-01-03 14:54:26 +0900

    fix: make format

---
+++ 05.wc/ls.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+
+# 横方向に配置する数
+HORIZONTAL_COUNT = 3
+
+def main
+  option = 0
+  params = setup
+  option = File::FNM_DOTMATCH if params.include?(:a)
+  files = Dir.glob('*', option, sort: true)
+  files = files.reverse if params.include?(:r)
+  if params.include?(:l)
+    create_row(files)
+  else
+    file_list_table = create_columns(files)
+
+    results = file_list_table.transpose
+    results.each { |result| puts result.join(' ') }
+  end
+end
+
+private
+
+def setup
+  opt = OptionParser.new
+  params = {}
+  opt.on('-a')
+  opt.on('-r')
+  opt.on('-l')
+  opt.parse(ARGV, into: params)
+  params
+end
+
+def create_columns(files)
+  files_count = files.length
+  # 縦方向に配置する数
+  vertical_count = files_count / HORIZONTAL_COUNT
+  vertical_count += 1 if files_count % HORIZONTAL_COUNT != 0
+
+  files.each_slice(vertical_count).map do |column|
+    column << nil while column.count < HORIZONTAL_COUNT
+    column
+  end
+end
+
+def create_row(files)
+  files.each do |file|
+    puts [
+      # ファイルタイプ
+      file_type(file),
+      # パーミション
+      permission(file),
+      # ハードリンクの数
+      File.stat(file).nlink.to_s.rjust(2),
+      # オーナー名
+      File.stat(file).uid.to_s.rjust(4),
+      # グループ名
+      File.stat(file).gid.to_s.rjust(4),
+      # バイトサイズ
+      File.stat(file).size.to_s.rjust(6),
+      # タイムスタンプ
+      File.stat(file).atime.strftime(' %b %d %H:%M'),
+      # ファイル名
+      " #{file}"
+    ].join
+  end
+end
+
+def file_type(file)
+  # ファイルタイプ
+  print({ file: '-', directory: 'd', link: 'l' }[File.ftype(file).to_sym])
+end
+
+def permission(file)
+  permissions = File.stat(file).mode.to_s(8)[-3..]
+  permissions.split('').each do |permission|
+    print({ '1': '--x', '2': '-w-', '3': '-wx', '4': 'r--', '5': 'r-x', '6': 'rw-', '7': 'rwx' }[permission.to_sym])
+  end
+  print ' '
+end
+
+main
